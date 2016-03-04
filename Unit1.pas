954d0887566ee6f4199623e69cb4b06118d48766
EvgenyKuksov <Nakaut47@Gmail.com>
2016-03-04 15:59:59 +0700

    Added files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -43,61 +43,66 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  ExcelApp := CreateOleObject('Excel.Application') as ExcelApplication; //Создаие документа Excel
-  ExcelApp.Workbooks.add(xlWBatWorkSheet,0);  //Создание рабочего листа
-  ExcelApp.Visible[0] := True;  //Открытие документа
-  Sheet := ExcelApp.Workbooks[1].WorkSheets[1] as ExcelWorksheet;
-  ExcelApp.Application.ReferenceStyle[0] := xlA1;
-
   Xn:=StrToFloat(Edit1.Text);
   Xk:=StrToFloat(Edit2.Text);
   Shag:=StrToFloat(Edit3.Text);
 
-//  Задаю значения "X"
-  col:='A';
-  x:=Xn;
-  Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='x';
-  row:=2;
-  while (x<=Xk) and (x>=Xn) do
+//  Проверка диапазона
+if Xn < Xk then
+  begin
+    ExcelApp := CreateOleObject('Excel.Application') as ExcelApplication; //Создаие документа Excel
+    ExcelApp.Workbooks.add(xlWBatWorkSheet,0);  //Создание рабочего листа
+    ExcelApp.Visible[0] := True;  //Открытие документа
+    Sheet := ExcelApp.Workbooks[1].WorkSheets[1] as ExcelWorksheet;
+    ExcelApp.Application.ReferenceStyle[0] := xlA1;
+
+    //  Задаю значения "X"
+    col:='A';
+    x:=Xn;
+    Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='x';
+    row:=2;
+    while (x<=Xk) and (x>=Xn) do
+      begin
+        Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=x;
+        x:=x+Shag;
+        row:=row+1;
+      end;
+
+  //  Задаю значения "Y"
+    col:='B';
+    x:=Xn;
+    Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='y';
+    row:=2;
+    while (x<=Xk) and (x>=Xn) do
     begin
-      Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=x;
+      if x>=1 then y:=power(cos(x),4);
+      if x<=-1 then y:=power(cos(x),5);
+      if (x>-2) and (x<2) then y:=1;
+      Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=y;
       x:=x+Shag;
       row:=row+1;
     end;
 
-//  Задаю значения "Y"
-  col:='B';
-  x:=Xn;
-  Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='y';
-  row:=2;
-  while (x<=Xk) and (x>=Xn) do
-  begin
-    if x>=1 then y:=power(cos(x),4);
-    if x<=-1 then y:=power(cos(x),5);
-    if (x>-2) and (x<2) then y:=1;
-    Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=y;
-    x:=x+Shag;
-    row:=row+1;
-  end;
+  sheet.Range['A2','B'+inttostr(row)].Select;
+  mshape:=Sheet.Shapes.AddChart(xlXYScatterSmoothNoMarkers,250,1,800,800);
+  mchart:=(mshape.Chart as ExcelChart).Location(xlLocationAsNewSheet,EmptyParam);
+  ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(1);
+  ExcelApp.Application.ActiveWorkbook.ActiveChart.ChartTitle[0].Text:='График функции';
+  MyDisp:=mchart.Axes(xlValue, xlPrimary, 0);
 
-sheet.Range['A2','B'+inttostr(row)].Select;
-mshape:=Sheet.Shapes.AddChart(xlXYScatterSmoothNoMarkers,250,1,800,800);
-mchart:=(mshape.Chart as ExcelChart).Location(xlLocationAsNewSheet,EmptyParam);
-ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(1);
-ExcelApp.Application.ActiveWorkbook.ActiveChart.ChartTitle[0].Text:='График функции';
-MyDisp:=mchart.Axes(xlValue, xlPrimary, 0);
+     mAxis:=Axis(MyDisp);
+     mAxis.HasTitle:=True;
+     mAxis.AxisTitle.Caption:='х';
 
-   mAxis:=Axis(MyDisp);
-   mAxis.HasTitle:=True;
-   mAxis.AxisTitle.Caption:='х';
+  MyDisp:=mchart.Axes(xlCategory, xlPrimary, 0);
 
-MyDisp:=mchart.Axes(xlCategory, xlPrimary, 0);
+     mAxis:=Axis(MyDisp);
+     mAxis.HasTitle:=True;
+     mAxis.AxisTitle.Caption:='Y';
 
-   mAxis:=Axis(MyDisp);
-   mAxis.HasTitle:=True;
-   mAxis.AxisTitle.Caption:='Y';
-
-ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(328);
+  ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(328);
+  end
+  else ShowMessage('Соблюдайте условие: "Xn < Xk"');
 end;
 
 end.
